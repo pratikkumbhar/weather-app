b25b28930200bf3f8e901218e3d71c6396dbe5a2
pratikkumbhar <pratikkumbhar83@gmail.com>
2024-09-25 08:27:43 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import {
+  GoogleMap,
+  useJsApiLoader,
+  StandaloneSearchBox,
+} from "@react-google-maps/api";
+import { useRef } from "react";
 function App() {
+  const inputref = useRef(null);
+  const { isLoaded } = useJsApiLoader({
+    id: "google-map-script",
+    googleMapsApiKey: "AIzaSyBU7Bg3ZG8pWsoUfG1nUrXg2IHpQyj8WNM",
+    libraries: ["places"],
+  });
+  console.log(isLoaded);
+  const handleOnPlacesChange = () => {
+    let address = inputref.current.getPlaces();
+    address.map((add) => {
+      console.log(add.address_components[0].long_name);
+    });
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="text-2xl underline">
+      {isLoaded && (
+        <StandaloneSearchBox
+          onLoad={(ref) => {
+            inputref.current = ref;
+          }}
+          onPlacesChanged={handleOnPlacesChange}>
+          <input placeholder="enter here"></input>
+        </StandaloneSearchBox>
+      )}
     </div>
   );
 }
