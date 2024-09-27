44741776a39f2da9726acd8f9e45f6e7a0d8f5f8
pratikkumbhar <pratikkumbhar83@gmail.com>
2024-09-27 17:19:28 +0530

    serarch

--- src/App.js
+++ src/App.js
@@ -4,26 +4,9 @@ import SearchBox from './components/search/SearchBox'
 
 
 export default function App() {
-  const [dark,setDark] = useState(false) 
-  useEffect(()=>{
-    if(dark){
-      document.querySelector('html')?.classList.add('dark')
-      console.log("dark is on")
-    }else{
-      document.querySelector('html')?.classList.remove('dark')
-      console.log("dark is off")
-    }
-  },[dark])
-
-  const darkModeHandler= ()=>{
-    setDark(!dark)
-  }
+  
   return (
-    <div>
-      <p className='text-2xl font-semibold font-poppins text-text dark:text-accent'>Hello my name is pratik </p>
-      <p className='text-2xl font-semibold font-roboto'>Hello my name is pratik </p>
-      <button className='bg-secondary text-text' onClick={darkModeHandler}>dark mode</button>
-      {/* <SearchBox></SearchBox> */}
-    </div>
+    <section className='h-screen w-full pt-10 bg-background'>
+   <SearchBox/></section>
   )
 }
--- src/components/search/SearchBox.js
+++ src/components/search/SearchBox.js
@@ -2,10 +2,12 @@ import {
   StandaloneSearchBox,
   useJsApiLoader,
 } from "@react-google-maps/api";
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
+import "./SearchBox.css";
 
 export default function SearchBox() {
-    const inputref = useRef(null);
+  const inputref = useRef(null);
+  const [location, setLocation] = useState("");
   const { isLoaded } = useJsApiLoader({
     id: "google-map-script",
     googleMapsApiKey: "AIzaSyBU7Bg3ZG8pWsoUfG1nUrXg2IHpQyj8WNM",
@@ -15,11 +17,26 @@ export default function SearchBox() {
   const handleOnPlacesChange = () => {
     let address = inputref.current.getPlaces();
     address.map((add) => {
-      console.log(add.address_components[0].long_name);
+      setLocation(add.address_components[0].long_name);
+      
     });
+    fetch(
+      "https://api.weatherapi.com/v1/current.json?key=bdc6d1ba112642c39ae155600242409&q=" +
+        location
+    )
+      .then((responce) => {
+        return responce.json();
+      })
+      .then((data) => {
+        console.log(data);
+      })
+      .catch((e) => {
+        console.log(e);
+      });
   };
   return (
-    <div className="p-10">
+    <div className="container h-screen flex flex-col items-center mx-auto ">
+      <h1 className="text-4xl text-text ">Weather App</h1>
       {isLoaded && (
         <StandaloneSearchBox
           onLoad={(ref) => {
@@ -27,9 +44,16 @@ export default function SearchBox() {
           }}
           onPlacesChanged={handleOnPlacesChange}
         >
-          <input placeholder="enter here"></input>
+          <input
+            type="text"
+            className="mt-10 px-4 py-3 w-[450px] rounded-md outline-none"
+            placeholder="location..."
+          />
         </StandaloneSearchBox>
       )}
+      <button className="text-text bg-accent py-2 px-4 rounded-lg mt-6 cursor-pointer">
+        Search
+      </button>
     </div>
   );
 }
